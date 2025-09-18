b368210368c3c1b96dcafb62fa43527f668c5fd4
Riva A. Crystal <92542234+rivacrystal@users.noreply.github.com>
2025-09-18 12:51:16 -0400

    Create capitalization.cpp

---
+++ capitalization.cpp
@@ -0,0 +1,24 @@
+// File: capitalization.cpp
+// Author: Riva A. Crystal
+// Date: September 18, 2025
+// String practice: Capitalize a sentence
+
+
+#include <iostream>
+#include <string>
+#include <cctype> // std::toupper
+
+int main() {
+    std::string s;
+
+    std::cout << "Enter a word or sentence: ";
+    getline(std::cin, s);
+
+    for (char& c : s) { // pass by reference because modifying
+        c = std::toupper(c);
+    }
+    
+    std::cout << "Uppercase version: " << s << std::endl;
+
+    return 0;
+}
